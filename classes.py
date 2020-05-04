d0990ad36141984fb26916f754ab11263bb70712
SamerBahri <samerbahri@gmail.com>
2020-05-04 14:45:25 -0700

    Main function created

--- classes.py
+++ classes.py
@@ -4,4 +4,17 @@ class Flight:
         self.orign = origin
         self.destination = destination
         self.duration = duration
-        
+
+
+def main():
+
+    #Create Flight
+    f = Flight(origin="New York", destination="Paris", duration=540)
+
+    #Change the Value of the variable
+    f.duration += 10
+
+    #print flight details
+    print(f.origin)
+    print(f.destination)
+    print(f.duration)
