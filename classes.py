c3bf0b879f178e179c32bc27c0956c809a3be23e
SamerBahri <samerbahri@gmail.com>
2020-05-04 15:25:03 -0700

    Created Passenger Class

--- classes.py
+++ classes.py
@@ -1,6 +1,17 @@
 class Flight:
 
+    counter = 1
+
     def __init__(self, origin, destination, duration):
+
+        #Keep track of id
+        self.id = Flight.counter
+        Flight.counter += 1
+
+        #Keep track of passengers
+        self.passengers = []
+
+        #details about flight
         self.origin = origin
         self.destination = destination
         self.duration = duration
@@ -11,19 +22,39 @@ class Flight:
         print(f"Flight destination: {self.destination}")
         print(f"Flight duration: {self.duration}")
 
+        #Print Passengers
+        print()
+        print("Passengers:")
+        for passenger in self.passengers:
+            print(f"{passenger.name}")
+
     def delay(self, amount):
         self.duration += amount
+
+    def add_passenger(self,p):
+        self.passengers.append(p)
+        p.flight_id = self.id
+
+class Passenger:
+
+    def __init__(self,name):
+        self.name = name
 
 
 def main():
 
     #Create Flight and print out its info
     f1 = Flight(origin="New York", destination="Paris", duration=540)
-    f1.delay(10)
-    f1.print_info()
 
-    f2 = Flight(origin="Tokyo", destination="Shanghai", duration=185)
-    f2.print_info()
+    #Create flights passengers
+    alice = Passenger(name="Alice")
+    bob = Passenger(name="Bob")
+
+    #Add Passengers
+    f1.add_passenger(alice)
+    f1.add_passenger(bob)
+
+    f1.print_info()
 
 if __name__ == "__main__":
     main()
